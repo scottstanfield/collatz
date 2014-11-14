78d2b6f703da328ce21320c9ea19bcd852beb462
Scott Stanfield <scott@vertigo.com>
2014-11-14 12:21:15 -0800

    initial commit

---
+++ collatz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python -t
+import sys
+from decimal import *
+
+def collatz(x):
+    seq = [x]
+
+    while x > 1:
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = 3*x + 1
+        seq.append(x)
+
+    return seq
+
+try:
+    start = int(Decimal(sys.argv[1]))
+except:
+    print "Usage: collatz starting-number (default is 10)"
+    start = 10
+
+seq = collatz(start)
+print seq
+print "collatz(%d) has %d items" % (start, len(seq))
+
